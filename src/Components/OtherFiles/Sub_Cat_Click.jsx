7ac267018dac65833a2e327cb443e9fc461e7479
MahrAbrarThind <mahrabrarthind@gmail.com>
2024-08-15 00:40:53 +0500

    Updated Code For SubCategories Page FilterContainer

--- src/Components/OtherFiles/Sub_Cat_Click.jsx
+++ src/Components/OtherFiles/Sub_Cat_Click.jsx
@@ -142,75 +142,79 @@ const Sub_Cat_Click = () => {
 
 
       {/* Conditional Rendering of Filter */}
-      <div ref={filterRef} className={showFilter ? "showFilterContainer" : "filter-container"}>
-        <h4>Filter Items</h4>
-        <div className="price-filter">
-          <input
-            type="number"
-            placeholder="Min Price"
-            value={minPrice}
-            min={0}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (value === '' || (Number(value) >= 0 && !value.includes('-'))) {
-                setMinPrice(value);
-              }
-            }}
-          />
-          <input
-            type="number"
-            placeholder="Max Price"
-            value={maxPrice}
-            min={0}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (value === '' || (Number(value) >= 0 && !value.includes('-'))) {
-                setMaxPrice(value);
-              }
-            }}
-          />
-          {name === 'clothes' && (
-            <div className="clothes-filter">
-              <p>Choose Stitched or Non-Stitched</p>
-              <select value={clothesStatus} onChange={(e) => setClothesStatus(e.target.value)}>
-                <option value="">All</option>
-                <option value="stitched">Stitched</option>
-                <option value="nonStitched">NonStitched</option>
-              </select>
-            </div>
-          )}
-          <button type='button' onClick={() => { handleFilter(); setShowFilter(!showFilter) }}>Filter</button>
-          <button type='button' onClick={() => { clearFilter(); setShowFilter(!showFilter) }}>Clear</button>
-        </div>
-      </div>
 
-      <div className="allproducts-container">
-        {loading ? (
-          <LoadingSpinner />
-        ) : (
-          filteredProducts.length === 0 ? (
-            <h3>Oops! No Products Have Been Added Yet</h3>
-          ) : (
-            filteredProducts?.map((product, index) => (
-              <div key={index} className="singleFeaturedProduct">
-                <NavLink to={`/product/${product?._id}`} className="featuredImgContainer">
-                  <img src={product.images[0].url} alt={product.title} />
-                </NavLink>
-                <p className='productTitle'>{product.title.substring(0, 80)}{product.title.length > 80 ? "..." : ""}</p>
-                <div className="productPrices">
-                  <p>Rs: {product.price}</p>
-                  <p>Rs: {product.comparedPrice}</p>
-                </div>
-                <button type='button' onClick={(e) => {
-                  e.preventDefault();
-                  handleAddToCart(product);
-                }}>
-                  Add to Cart
-                </button>
+      <div className="filterAndProductsContainer">
+
+        <div ref={filterRef} className={showFilter ? "showFilterContainer" : "filter-container"}>
+          <h4>Filter Items</h4>
+          <div className="price-filter">
+            <input
+              type="number"
+              placeholder="Min Price"
+              value={minPrice}
+              min={0}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (value === '' || (Number(value) >= 0 && !value.includes('-'))) {
+                  setMinPrice(value);
+                }
+              }}
+            />
+            <input
+              type="number"
+              placeholder="Max Price"
+              value={maxPrice}
+              min={0}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (value === '' || (Number(value) >= 0 && !value.includes('-'))) {
+                  setMaxPrice(value);
+                }
+              }}
+            />
+            {name === 'clothes' && (
+              <div className="clothes-filter">
+                <p>Choose Stitched or Non-Stitched</p>
+                <select value={clothesStatus} onChange={(e) => setClothesStatus(e.target.value)}>
+                  <option value="">All</option>
+                  <option value="stitched">Stitched</option>
+                  <option value="nonStitched">NonStitched</option>
+                </select>
               </div>
-            ))
-          )
-        )}
+            )}
+            <button type='button' onClick={() => { handleFilter(); setShowFilter(!showFilter) }}>Filter</button>
+            <button type='button' onClick={() => { clearFilter(); setShowFilter(!showFilter) }}>Clear</button>
+          </div>
+        </div>
+
+        <div className="allproducts-container">
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            filteredProducts.length === 0 ? (
+              <h3>Oops! No Products Have Been Added Yet</h3>
+            ) : (
+              filteredProducts?.map((product, index) => (
+                <div key={index} className="singleFeaturedProduct">
+                  <NavLink to={`/product/${product?._id}`} className="featuredImgContainer">
+                    <img src={product.images[0].url} alt={product.title} />
+                  </NavLink>
+                  <p className='productTitle'>{product.title.substring(0, 80)}{product.title.length > 80 ? "..." : ""}</p>
+                  <div className="productPrices">
+                    <p>Rs: {product.price}</p>
+                    <p>Rs: {product.comparedPrice}</p>
+                  </div>
+                  <button type='button' onClick={(e) => {
+                    e.preventDefault();
+                    handleAddToCart(product);
+                  }}>
+                    Add to Cart
+                  </button>
+                </div>
+              ))
+            )
+          )}
+        </div>
       </div>
     </div>
   );
